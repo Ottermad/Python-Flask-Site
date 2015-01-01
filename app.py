b59ff88a46cb0726408ac7b234187ce581ed5cbd
Ottermad <charlie.thomas@attwoodthomas.net>
2015-01-01 18:45:15 +0000

    Cleaned up code and added README.md

--- app.py
+++ app.py
@@ -1,5 +1,24 @@
-from flask import *
-from db_functions import *
+# Main file for blog
+
+
+# Import statements
+from flask import (
+    Flask,
+    render_template,
+    redirect,
+    Markup,
+    request,
+    flash,
+    url_for,
+    session
+)
+from db_functions import (
+    get_posts,
+    get_post,
+    add_post,
+    update_post,
+    delete_post,
+)
 import markdown
 
 app = Flask(__name__)
@@ -7,82 +26,176 @@ app.config["SECRET_KEY"] = "some_really_long_random_string_here"
 app.config["USERNAME"] = "charliethomas"
 app.config["PASSWORD"] = "my_password"
 
+# Routes
+
+
 @app.route("/")
 @app.route("/show")
 def show():
-	context = {
-		"posts": get_posts()[::-1]
-	}
-	return render_template("show.html", **context)
+    """Main Page for blog. It shows all the posts from the db.
+
+    Parameters:
+    None
+
+    Template: show.html
+    Redirect: None
+    """
+    context = {
+        "posts": get_posts()[::-1]
+    }
+    return render_template("show.html", **context)
+
 
 @app.route("/post/<id>")
 def post(id):
-	post = get_post(id) 
-	title =  post["title"]
-	body = post["body"]
-	unicode_body = body.decode("utf-8")
-	html_body = markdown.markdown(unicode_body)
-	safe_html_body = Markup(html_body)
-	context = {
-		"title": post["title"],
-		"body": safe_html_body
-	}
-	return render_template("post.html", **context)
+    """Page for each post. It shows the title and body of a given post.
+
+    Parameters:
+    id - init - id for post to view
+
+    Template: post.html
+    Redirect: None
+    """
+    post = get_post(id)
+    title = post["title"]
+    body = post["body"]
+    unicode_body = body.decode("utf-8")
+    html_body = markdown.markdown(unicode_body)
+    safe_html_body = Markup(html_body)
+    context = {
+        "title": title,
+        "body": safe_html_body
+    }
+    return render_template("post.html", **context)
 
 
 @app.route("/update/<id>", methods=["POST", "GET"])
 def update(id):
-	if request.method == "POST":
-		result = update_post(
-			id,
-			request.form["title"],
-			request.form["body"]
-		)
-		flash(result)
-		return redirect(url_for("show"))
-	else:
-		post = get_post(id)
-		return render_template("update.html", **post)
+    """Route to update post the function has two operations based on the
+    request method.
+
+    Parameters:
+    id - init - id for post to update
+
+    GET method:
+    If the request method is GET it loads the form to update the post.
+
+    Template: edit.html
+    Redirect: None
+
+    POST method:
+    If the request method is POST then it updates the post based on the id
+    with the title and body.
+
+    Template: None
+    Redirect: show
+    """
+    if request.method == "POST":
+        result = update_post(
+            id,
+            request.form["title"],
+            request.form["body"]
+        )
+        flash(result)
+        return redirect(url_for("show"))
+    else:
+        post = get_post(id)
+        return render_template("edit.html", **post)
 
 
 @app.route("/delete/<id>")
 def delete(id):
-	result = delete_post(id)
-	flash(result)
-	return redirect(url_for("show"))
+    """Route to delete post from id.
+
+    Parameters:
+    id - int - id for post to delete
+
+    Template: None
+    Redirect: show
+    """
+    result = delete_post(id)
+    flash(result)
+    return redirect(url_for("show"))
+
 
 @app.route("/add", methods=["POST", "GET"])
 def add():
-	if request.method == "POST":
-		result = add_post(
-			request.form["title"],
-			request.form["body"]
-		)
-		flash(result)
-		return redirect(url_for("show"))
-	else:
-		return render_template("add.html")
+    """Route to add post. The function has two operations based on the request
+    method
+
+    Parameters:
+    None
+
+    GET method:
+    If the request method is GET it loads the form to add a post.
+
+    Template: add.html
+    Redirect: None
+
+    POST method:
+    If the request method is POST then it adds the post with the title and
+    body.
+
+    Template: None
+    Redirect: show
+    """
+    if request.method == "POST":
+        result = add_post(
+            request.form["title"],
+            request.form["body"]
+        )
+        flash(result)
+        return redirect(url_for("show"))
+    else:
+        return render_template("add.html")
+
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
-	error = None
-	if request.method == 'POST':
-		if request.form['username'] != app.config['USERNAME']:
-			error = 'Invalid username'
-		elif request.form['password'] != app.config['PASSWORD']:
-			error = 'Invalid password'
-		else:
-			session['logged_in'] = True
-			flash('You were logged in sucessfully')
-			return redirect(url_for('show'))
-	return render_template('login.html', error=error)
+    """Route to login. The function has two operations based on the request
+    method
+
+    GET method:
+    If the request method is GET it loads the form to login.
+
+    Template: login.html
+    Redirect: None
+
+    POST method:
+    If the request method is POST then it trys tp log the user in. If login
+    errors it returns an errors back to login.html. If login is successful
+    it redirects to show.
+
+    Template: None
+    Redirect: show
+    """
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in sucessfully')
+            return redirect(url_for('show'))
+    return render_template('login.html', error=error)
+
 
 @app.route('/logout')
 def logout():
-	session.pop('logged_in', None)
-	flash('You were logged out')
-	return redirect(url_for('show'))
+    """Route to logout.
+
+    Parameters:
+    None
+
+    Template: None
+    Redirect: show
+    """
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show'))
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
--- db_functions.py
+++ db_functions.py
@@ -13,8 +13,8 @@ db = MySQLDatabase("BLOG_DB", user="root", passwd="OttersR0ck")
 class BaseModel(Model):
     """A model/class to act as a base for other models.
 
-    This is to keep my code DRY by setting common elements in one place
-    e.g. the db
+    This is to keep my code DRY by setting common elements in one place e.g.
+    the db
     """
     class Meta:
         database = db
@@ -30,20 +30,37 @@ class Post(BaseModel):
 
 
 def setup_db():
+    """Function to connect to db and create tables"""
     db.connect()
     db.create_tables([Post])
 
 
 def add_post(title, body):
+    """Function to create a new post
+
+    Parameters:
+    title - string -title for the posts
+    body - string - main content of the post
+
+    Returns Success if post creation is successful or Error if not.
+    """
     try:
         new_post = Post(title=title, body=body)
         new_post.save()
         return "Success"
     except:
-       return "Error"
+        return "Error"
 
 
 def get_posts():
+    """Function to get all posts from db
+
+    Parameters
+    None
+
+    Returns a list of dictionaries. The dictionaries will be in the following
+    have an id, title and body
+    """
     posts = []
     for post in Post.select():
         print post, post.id, post.title, post.body
@@ -57,6 +74,14 @@ def get_posts():
 
 
 def get_post(id):
+    """Function to get an indiviual post from an id.
+
+    Parameters
+    id - int - id for a post
+
+    It searches for the post with corrosponding id and returns a dictionary
+    with the id, title and body of the post.
+    """
     post_dict = {}
     post = Post.get(Post.id == id)
     post_dict = {
@@ -68,6 +93,16 @@ def get_post(id):
 
 
 def update_post(id, title, body):
+    """Function to update a post
+
+    Parameters
+    id - int - id for post to update
+    title - string - new title for post
+    body - string - new body for post
+
+    It searches for the post with the matching id. Then updates the title and
+    body.
+    """
     try:
         post = Post.get(Post.id == id)
         post.title = title
@@ -79,10 +114,16 @@ def update_post(id, title, body):
 
 
 def delete_post(id):
+    """Function to delete a post
+
+    Parameters
+    id - int - id for post to delete
+
+    It searches for the post with the matching id then deletes it.
+    """
     try:
         post = Post.get(Post.id == id)
         post.delete_instance()
         return "Success"
     except:
         return "Error"
-
